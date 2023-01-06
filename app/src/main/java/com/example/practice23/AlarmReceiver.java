26449c2d0278706718f2df7e3b40e99b375dca44
NagarjunaLoka <knagarjun43@gmail.com>
2023-01-06 14:16:00 +0530

    frst commit

---
+++ app/src/main/java/com/example/practice23/AlarmReceiver.java
@@ -0,0 +1,36 @@
+package com.example.practice23;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    @Override
+    // implement onReceive() method
+    public void onReceive(Context context, Intent intent) {
+
+        // we will use vibrator first
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(4000);
+
+        Toast.makeText(context, "Alarm! Wake up! Wake up!", Toast.LENGTH_LONG).show();
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null) {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+
+        // setting default ringtone
+        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+    }
+}
